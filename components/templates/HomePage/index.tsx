acc091fefaf0c9a0cd8dd3242f0365d95239574f
usaidmasud <syaripmasud@gmail.com>
2022-06-10 14:27:25 +0800

    modify homePage

--- components/templates/HomePage/index.tsx
+++ components/templates/HomePage/index.tsx
@@ -1,14 +1,20 @@
+import { useRouter } from "next/router";
 import React from "react";
 import Footer from "../../moleculs/Footer";
 import Header from "../../moleculs/Header";
+import HeaderMini from "../../moleculs/HeaderMini";
 
 interface Props {
   children: React.ReactNode;
 }
 const HomePage: React.FC<Props> = ({ children }) => {
+  const router = useRouter();
+
   return (
     <div className="font-nunito bg-gray-50">
-      <Header />
+      {
+        router.pathname === "/" ? <Header /> : <HeaderMini />
+      }
       {children}
       <Footer />
     </div>
