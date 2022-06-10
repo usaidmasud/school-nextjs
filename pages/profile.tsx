25b00bc8c3e9d97e2148a4882ca162dd4027b615
usaidmasud <syaripmasud@gmail.com>
2022-06-10 14:27:15 +0800

    modify profile pagea

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from "next";
-import React from "react";
-import { SchoolProfile } from "../components/moleculs";
+import { SchoolProfile, TablePendidik } from "../components/moleculs";
 import { HomePage } from "../components/templates";
 const Profile: NextPage = () => {
   return (
     <HomePage>
       <SchoolProfile/>
+      <TablePendidik />
     </HomePage>
   );
 };
